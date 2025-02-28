8ab89b50a389dc7872e8c882f51fc4160ed3f6a8
Jinwoo Kim <jinunemo@gmail.com>
2025-02-28 20:22:57 +1000

    add load data

--- backend/businesses/urls.py
+++ backend/businesses/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_dashboard_data
 
 urlpatterns = [
-    path("dashboard/", get_dashboard_data, name="dashboard-data"),
+    path("api/dashboard/", get_dashboard_data, name="dashboard-data"),
 ]
