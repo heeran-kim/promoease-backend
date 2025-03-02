c31bd21a70a438f2a2f606cb511212c8e8f84497
Heeran Kim <heerankim3@gmail.com>
2025-03-02 17:26:05 +1000

    [posts][create]

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import show
+from .views import show, create
 
 urlpatterns = [
     path("", show, name="show"),
+    path("new/", create, name="create"),
 ]
