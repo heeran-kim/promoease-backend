f968f1cc6b8b9fe361464fa1fef15b0a4677f17d
Heeran Kim <heerankim3@gmail.com>
2025-03-04 08:10:19 +1000

    [businesses] change attribute name from type to category and set some attributes nullable

--- backend/businesses/models.py
+++ backend/businesses/models.py
@@ -3,10 +3,10 @@ from users.models import User
 
 class Business(models.Model):
     name = models.CharField(max_length=255)
-    logo = models.ImageField(upload_to='business_logos/', blank=True, null=True)
-    type = models.CharField(max_length=255)  # Store the type of business
-    target = models.CharField(max_length=255)  # Store target audience (if applicable)
-    vibe = models.CharField(max_length=255)  # Store vibe or theme of the business
+    logo = models.ImageField(upload_to='logos/', blank=True, null=True)
+    category = models.CharField(max_length=255, blank=True, null=True)  # Store the category of business
+    target = models.CharField(max_length=255, blank=True, null=True)  # Store target customer
+    vibe = models.CharField(max_length=255, blank=True, null=True)  # Store vibe or theme of the business
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="businesses")
     created_at = models.DateTimeField(auto_now_add=True)
 
